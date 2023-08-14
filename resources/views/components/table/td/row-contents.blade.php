01c252a5881725f62d5a661b7bf0ab76677e4450
David <david.massanek@devront.com>
2023-08-14 11:06:48 +0200

    livewire v3

--- resources/views/components/table/td/row-contents.blade.php
+++ resources/views/components/table/td/row-contents.blade.php
@@ -22,7 +22,7 @@
         >
             @if (! $hidden)
                 <button
-                    x-on:click.prevent="$dispatch('toggle-row-content', {'row': {{ $rowIndex }}});open = !open"
+                    x-on:click.prevent="$wire.$dispatch('toggle-row-content', {'row': {{ $rowIndex }}});open = !open"
                 >
                     <svg x-show="!open" xmlns="http://www.w3.org/2000/svg"  class="text-green-600 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -49,7 +49,7 @@
         >
             @if (! $hidden)
                 <button
-                    x-on:click.prevent="$dispatch('toggle-row-content', {'row': {{ $rowIndex }}});open = !open"
+                    x-on:click.prevent="$wire.$dispatch('toggle-row-content', {'row': {{ $rowIndex }}});open = !open"
                     class="p-0"
                     style="background:none;border:none;"
                 >
